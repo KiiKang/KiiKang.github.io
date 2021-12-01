05d026c6155eafa66ca8e3d92e5de49d02f417ae
KiiKang <63621971+KiiKang@users.noreply.github.com>
2021-12-01 15:24:53 -0500

    update style
    add waveform
    add audio length encoding

--- soundscape_constellation/d3.js
+++ soundscape_constellation/d3.js
@@ -1,7 +1,12 @@
+// get window size
+var plotDiv = document.getElementById("plot");
+var windowWidth = plotDiv.clientWidth;
+var windowHeight = plotDiv.clientHeight;
+
 // set the dimensions and margins of the graph
-const margin = {top: 10, right: 30, bottom: 30, left: 60},
-    width = 800 - margin.left - margin.right,
-            height = 800 - margin.top - margin.bottom;
+const margin = {top: 60, right: 100, bottom: 60, left: 240},
+    width = windowWidth - margin.left - margin.right,
+    height = windowHeight - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 const svg = d3.select("#plot")
@@ -12,11 +17,10 @@ const svg = d3.select("#plot")
         .attr("transform",
         "translate(" + margin.left + "," + margin.top + ")");
 
-let jsonRequest = new Request('./json/hfc.json');
+let jsonRequest = new Request('./json/hfc_.json');
 
 // node js
 // const fs = Require('fs');
-
 
 function ConnectedScatterPlot(data, mapMethod) {
     var dataFiltered = data.filter( d => {return d.mapMethod === mapMethod})[0]
@@ -42,8 +46,9 @@ function ConnectedScatterPlot(data, mapMethod) {
                     res = res.concat(xy.x);
                 });
                 return(res);
-            }(dataFiltered.coor)))
+            }(dataFiltered.pts)))
         .range([0, width]);
+
     // Y scale
     let y = d3.scaleLinear()
         .domain(d3.extent(
@@ -52,28 +57,46 @@ function ConnectedScatterPlot(data, mapMethod) {
                     res = res.concat(xy.y);
                 });
                 return(res);
-            }(dataFiltered.coor)))
+            }(dataFiltered.pts)))
         .range([0, height]);
+
     // r scale
-    // let r = d3.scaleLinear()
-    //     .domain([0, 1000])
-    //     .range([0, 10]);
+    let r = d3.scaleLinear()
+        .domain([0, 1500])
+        .range([2, 17]);
+
     // Add paths
     let line = svg.append("path")
-        .datum(dataFiltered.coor)
+        .datum(dataFiltered.pts)
         .attr("fill", "none")
         .attr("class", "line")
-        .attr("stroke", 'black')
-        .attr("opacity", 0.5)
+        .attr("stroke", 'white')
         .attr("stroke-width", 0.5)
+        .attr("opacity", 0.3)
+        .attr("stroke-dasharray", "3,3")
         .attr("d", d3.line()
             .x(d => { return x(d.x); })
             .y(d => { return y(d.y); })
         );
     // Add dots
-    let dot = svg.append('g')
+    let dot_bg = svg.append('g')
         .selectAll("dot")
-        .data(dataFiltered.coor)
+        .data(dataFiltered.pts)
+        .enter()
+        .append("circle")
+        .attr("class", "dot_bg")
+        .attr("cx", d => {
+            return parseInt(x(d.x));
+        })
+        .attr("cy", d => {
+            return parseInt(y(d.y));
+        })
+        .attr("r", d => {
+            return 10+4*r(d.endTime - d.startTime);
+        })
+    let dot = svg.append('g')
+        .selectAll("dot_bg")
+        .data(dataFiltered.pts)
         .enter()
         .append("circle")
         .attr("class", "dot")
@@ -83,10 +106,13 @@ function ConnectedScatterPlot(data, mapMethod) {
         .attr("cy", d => {
             return parseInt(y(d.y));
         })
-        .attr("r", 5)
+        .attr("r", d => {
+            return r(d.endTime - d.startTime);
+        })
         .attr("fill", 'white')
-        .attr("stroke", 'black')
-        .attr("stroke-width", 2);
+        .attr("opacity", 0.5)
+        .attr("stroke", 'white')
+        .attr("stroke-width", 0.5);
 
     // A function that update the chart
 
@@ -103,7 +129,7 @@ function ConnectedScatterPlot(data, mapMethod) {
                         res = res.concat(xy.x);
                     });
                     return(res);
-                }(dataFiltered_.coor)))
+                }(dataFiltered_.pts)))
             .range([0, width]);
         // Add Y axis
         y = d3.scaleLinear()
@@ -113,12 +139,12 @@ function ConnectedScatterPlot(data, mapMethod) {
                         res = res.concat(xy.y);
                     });
                     return(res);
-                }(dataFiltered_.coor)))
+                }(dataFiltered_.pts)))
             .range([0, height]);
 
         // Give these new data to update line
         line
-            .datum(dataFiltered_.coor)
+            .datum(dataFiltered_.pts)
             .transition()
             .duration(1500)
             .attr("d", d3.line()
@@ -126,11 +152,19 @@ function ConnectedScatterPlot(data, mapMethod) {
                 .y( d => { return y(d.y); })
             )
         dot
-            .data(dataFiltered_.coor)
+            .data(dataFiltered_.pts)
             .transition()
             .duration(1500)
             .attr("cx", d => { return x(d.x); })
             .attr("cy", d => { return y(d.y); })
+
+        dot_bg
+            .data(dataFiltered_.pts)
+            .transition()
+            .duration(1500)
+            .attr("cx", d => { return x(d.x); })
+            .attr("cy", d => { return y(d.y); })
+
     }
 
     d3.select("#methodDropDown").on("change", function() {
@@ -139,7 +173,6 @@ function ConnectedScatterPlot(data, mapMethod) {
     })
 
 };
-
 
 fetch(jsonRequest)
     .then(response => response.json())
