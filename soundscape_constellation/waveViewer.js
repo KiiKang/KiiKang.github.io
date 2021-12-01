44b547ddaac599b4c378f9665fbd9c656c6ed5b1
KiiKang <63621971+KiiKang@users.noreply.github.com>
2021-12-01 15:29:39 -0500

    add waveViewer

---
+++ soundscape_constellation/waveViewer.js
@@ -0,0 +1,36 @@
+// https://github.com/katspaugh/wavesurfer.js/tree/master/example
+
+
+var wavesurfer;
+
+document.addEventListener('DOMContentLoaded', function() {
+    wavesurfer = WaveSurfer.create({
+        // your options here
+        container: document.querySelector('#waveform'),
+        waveColor: 'grey',
+        vertical: true,
+        minPxPerSec: 30,
+    });
+
+    wavesurfer.on('error', function(e) {
+        console.warn(e);
+    })
+
+    wavesurfer.load('media/birds.wav');
+
+    document
+        .querySelector('[data-action="play"]')
+        .addEventListener('click', wavesurfer.playPause.bind(wavesurfer));
+
+})
+
+
+// const colormap = require('colormap');
+// const colors = colormap({
+//     colormap: 'hot',
+//     nshades: 256,
+//     format: 'float'
+// });
+//
+// const fs = require('fs');
+// fs.writeFile('hot-colormap.json', JSON.stringify(colors));
