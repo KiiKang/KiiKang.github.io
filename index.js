512a773bc68160099ff531f672704fc58529a135
KiiKang <63621971+KiiKang@users.noreply.github.com>
2022-03-23 23:00:53 -0400

    update
    add project descriptions

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 let divWorks = document.getElementById("works");
 let divBio = document.getElementById("bio");
-console.log(divWorks);
+
 function showDiv(divID) {
     divWorks.style.display = "none";
     divBio.style.display = "none";
     document.getElementById(divID).style.display = "block";
 };
+
+let works = document.getElementsByClassName("work");
+let descriptions = document.getElementsByClassName("description");
+for (let i=0; i<works.length; i++){
+    works[i].addEventListener("click", function(){
+        for (const div of descriptions){
+            div.style.display = "none";
+        }
+        descriptions[i].style.display = "block";
+    })
+}
