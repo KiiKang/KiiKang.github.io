edd657b5f2ba083162f67e49f7bc12b5c1b9745b
KiiKang <63621971+KiiKang@users.noreply.github.com>
2022-03-30 14:50:38 -0400

    update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function showDiv(divID) {
     divWorks.style.display = "none";
     divBio.style.display = "none";
     document.getElementById(divID).style.display = "block";
-};
+}
 
 let works = document.getElementsByClassName("work");
 let descriptions = document.getElementsByClassName("description");
@@ -17,3 +17,25 @@ for (let i=0; i<works.length; i++){
         descriptions[i].style.display = "block";
     })
 }
+
+window.addEventListener('resize', reArrange);
+
+function reArrange(){
+    let wrapper = document.getElementById("container");
+    let divLeft = document.getElementById("left");
+    let divRight = document.getElementById("right");
+    let overlays = document.querySelectorAll(".media-overlays");
+    if (window.innerHeight > window.innerWidth) {
+        wrapper.style.flexDirection = 'column';
+        divLeft.style.width = '100vw';
+        divRight.style.width = '100vw';
+        for (const div in overlays){
+            div.style.fontSize = '50px';
+        }
+    } else {
+        wrapper.style.flexDirection = 'row';
+        divLeft.style.width = '28vw';
+        divRight.style.width = '72vw';
+    }
+}
+
